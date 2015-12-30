cae5d63ea006bb2c6c3be9ab142f0d8c61323b7e
luxtmxw <luxtmxw@126.com>
2015-12-30 19:47:37 +0800

    初始化项目

---
+++ MXWPersonalDemo/MXWPersonalDemo/MXWPersonalDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXWPersonalDemo
+//
+//  Created by lanou3g on 15/12/30.
+//  Copyright © 2015年 mxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
